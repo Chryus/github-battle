b9dd9576e969f8cb87ae613893d259573f522162
Chris <chris.haack@me.com>
2017-01-08 12:06:14 -0500

    conditionally update start scripts for dev/prod, config server.js to serve assets and app from dist/, add minification/uglification to webpack, add compression to express

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   filename: 'index.html',
   inject: 'body'
 })
+var webpack = require('webpack')
 
 module.exports = {
   entry: [
@@ -11,12 +12,19 @@ module.exports = {
   ],
   output: {
     path: __dirname + '/dist',
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
+    publicPath: "/"
   },
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react'}
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    HtmlWebpackPluginConfig
+  ] : [HtmlWebpackPluginConfig],
+
 }
