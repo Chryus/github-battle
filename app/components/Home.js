ad7dedf9e68048d6e21110846d0525f30bfe25a2
Chris <chris.haack@me.com>
2017-01-09 14:35:07 -0500

    add button to index to link to playerOne component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from '../styles'
+import { ReactRouter, Link } from 'react-router'
+
 
 export default React.createClass({
   render() {
     return (
-      <div>Hello from Home</div>
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <p className="lead">Some fancy motto</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>
+            Get Started
+          </button>
+        </Link>
+      </div>
     )
   }
 })
