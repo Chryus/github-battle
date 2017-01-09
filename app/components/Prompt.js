6b5aa94fa8c63f570f7d2d77b95bce1be9de61b1
Chris <chris.haack@me.com>
2017-01-09 17:57:14 -0500

    refactor as stateless functional component and set prop types

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import styles from '../styles'
+let PropTypes = React.PropTypes
 
-export default React.createClass({
-  render() {
-    return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={styles.transparentBg}>
-        <h1>{this.props.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.props.onSubmitUser}>
-            <div className="form-group">
-              <input 
-                className="form-control" 
-                placeholder="Github Username"
-                type="text"
-                onChange={this.props.onUpdateUser} 
-                value={this.props.username} />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Continue
-              </button>
-            </div>
-          </form>
-        </div>
+
+function Prompt(props) {
+  return (
+    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={styles.transparentBg}>
+      <h1>{props.header}</h1>
+      <div className="col-sm-12">
+        <form onSubmit={props.onSubmitUser}>
+          <div className="form-group">
+            <input 
+              className="form-control" 
+              placeholder="Github Username"
+              type="text"
+              onChange={props.onUpdateUser} 
+              value={props.username} />
+          </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-success"
+              type="submit">
+                Continue
+            </button>
+          </div>
+        </form>
       </div>
-    ) 
-  }
-});
+    </div>
+  );
+}
+
+Prompt.propTypes = {
+  header: PropTypes.string.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  onSubmitUser: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
+}
+
+export default Prompt;
