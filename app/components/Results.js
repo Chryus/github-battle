a04adf747abf7e2fa6221f27f81af00572a3b3f7
Chris <chris.haack@me.com>
2017-01-18 19:04:39 -0500

    build out presentation

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import UserDetails from './UserDetails'
+import UserDetailsWrapper from './UserDetailsWrapper'
+
 const PropTypes = React.PropTypes;
 
 function puke(object) {
@@ -6,8 +9,20 @@ function puke(object) {
 }
 
 function Results(props) {
+  const winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  const losingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div>{puke(props)}</div>
+    <div className="jumbotron col-sm-12 text-center transparentBg">
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}/>
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}/>
+        </UserDetailsWrapper>
+      </div>
+    </div>
   )
 }
 
